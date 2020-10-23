79d99a8197f0e64cf9d54469963ad27b0ea0bb64
Ty <tptorres5@gmail.com>
2020-10-22 22:22:41 -0400

    Testing upload

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/Link'
+const Index = () => (
+  <div>
+    <h1>Hello</h1>
+    <h1><Link href="/about"><a>About Page</a></Link></h1>
+  </div>
+);
+
+export default Index;
